fe144c04a686121462bf60fa7f84bc08f2314fe9
DonaldDoomer <jeremie.bourdet@u-psud.fr>
2021-01-18 16:32:04 +0100

    download fastq et mb5

---
+++ dl.sh
@@ -0,0 +1,20 @@
+IFS=$'\n'
+for line in $(cat $2)
+do
+  IFS=$'\t;' read -r -a array <<< "$line"
+  if [ ${array[2]} != "fastq_ftp" ]
+  then
+    boucle=true
+    while [ $boucle ]
+    do
+      filename=$(basename "ftp://"${array[3]})
+      printf "Téléchargement de : %s \n" $filename
+      wget -P $1  -nc --no-clobber "ftp://"${array[3]}
+      $boucle = [${array[1]} != md5sum $filename]
+      if [ $boucle ]
+      then
+        rm $filename
+      fi
+    done
+  fi
+done
