76bba41059682abad2ae67e2d35cc59f6a94be47
DonaldDoomer <jeremie.bourdet@u-psud.fr>
2021-02-01 17:15:37 +0100

    bwa --> sam

--- dl.sh
+++ dl.sh
@@ -6,31 +6,29 @@ IFS=$'\n'
 for line in $(cat $2)
 do
   IFS=$'\t' read -r -a array <<< "$line" #Divise ligne en un tableau,le séparateur est \n, la tabulation
-  if [ "${array[2]}" != "fastq_ftp" ] && [ "${array[2]}" != "" ] #On zappe la première ligne et les lignes vides
+  if [ "${array[3]}" != "fastq_ftp" ] && [ "${array[3]}" != "" ] #On zappe la première ligne et les lignes vides
   then
-    i=1   # i sert à couper la bonne partie d'une case du genre ;~~~;~~~;~~~;~~~;~~~
-    path=$(echo ${array[2]}";stop" | cut -d';' -f $i) #Rajout de "stop" à la fin pour pouvoir savoir qu'on a lu toute la case
-    while [ "$path" != "stop" ]                       #Sinon
-     do
-      failed_md5=1 #True
-      while [ "$failed_md5" == 1 ] #Tant que le ficher n'est pas reçu correctement
-      do
-        filename=$(basename $path)
-        printf "Téléchargement de : %s \n" $filename
-        wget -P $1  -nc --no-clobber "ftp://"$path #marche stp
-        md5=$(md5sum $1'/'$filename | cut -d' ' -f 1) #md5sum rajoute le nom du ficher en plus du hash code--> on ne garde que ce dernier
-        failed_md5=$([ "echo ${array[1]} | cut -d';' -f $i" != "$md5" ])
+    IFS=$';' read -r -a nb_read <<< "${array[3]}" #Divise la case en un tableau,le séparateur est;
+    for ((i=0; i<"${#nb_read[@]}"; i++)) # ${#nb_read[@]} la taille du tableau
+    do
+        path="${nb_read[i]}"
+        failed_md5=1 #True
+        while [ "$failed_md5" == 1 ] #Tant que le ficher n'est pas reçu correctement
+        do
+          filename=$(basename $path)
+          printf "Téléchargement de : %s \n" $filename
+          wget -P $1  -nc --no-clobber "ftp://"$path #marche stp
+          md5=$(md5sum $1'/'$filename | cut -d' ' -f 1) #md5sum rajoute le nom du ficher en plus du hash code--> on ne garde que ce dernier
+          failed_md5=$([ "echo ${array[2]} | cut -d';' -f $i" != "$md5" ])
 
-        if [ "$failed_md5" == 1 ]
-        then
-          printf "Erreur dans le téléchargement de : %s \n" $filename
-          rm $1'/'$filename
-        else
-          printf "Fin du téléchargement de : %s \n" $filename
-        fi
+          if [ "$failed_md5" == 1 ]
+          then
+            printf "Erreur dans le téléchargement de : %s \n" $filename
+            rm $1'/'$filename
+          else
+            printf "Fin du téléchargement de : %s \n" $filename
+          fi
+        done
       done
-      ((i++))     #(()) indique une opération arithmétique
-      path=$(echo ${array[2]}";stop" | cut -d';' -f $i)   #Pas de do while en bash ...
-    done
-  fi
+    fi
 done
