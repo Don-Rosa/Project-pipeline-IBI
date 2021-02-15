446ef3f3e0f839eca55ba2786e42d726697de3d4
DonaldDoomer <jeremie.bourdet@u-psud.fr>
2021-02-15 10:09:01 +0100

    analyse de cov

---
+++ cov.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+dir=$1
+tsv=$2                 # On explicite les parametres
+
+IFS=$'\n'
+for line in $(cat $tsv)
+do
+  filename=$(echo "$line" | cut -d$'\t' -f 1)
+  min=99999  #de la merde, à changer
+  max=0
+  total=0
+  nb_lines=0
+  echo "$filename"
+  for line_cov in $(cat $dir'/'"$filename"_cov)
+  do
+     IFS=$'\t' read -r -a cov <<< "$line_cov" #Gros gain de performance à ne pas utiliser cut et |
+    ((nb_lines++))
+    ((total += "${cov[3]}"))
+    if (("${cov[3]}" > $max))
+    then
+      max="${cov[3]}"
+    fi
+    if (("${cov[3]}" < $min))
+    then
+      min="${cov[3]}"
+    fi
+  done
+  average=$(echo "$total / $nb_lines" | bc -l)
+  echo "$filename done"
+  echo "Coverage of $filename : Average = $average  min $min max $max" >> "$dir"/Résultats/Cov.txt
+done
